7237d4f80929a3d07427dc9f1ea9fddcefbc7638
Nicky Out <nicky.out@webprint.nl>
2017-09-04 02:37:06 +0200

    Added stuff to write relative results, untested

--- index.js
+++ index.js
@@ -1,46 +1,76 @@
-var BenchReporter = function(baseReporterDecorator) {
+var RelativeResultSaver = require('./lib/relative-result-saver');
+var resultToRelativeResult = require('./lib/result-to-relative-result');
+var path = require('path');
+
+var BenchReporter = function(baseReporterDecorator, config) {
   baseReporterDecorator(this);
 
   var resultSet = {};
+  var options = config.benchmarkReporter || {};
+  var excludeFromFastest = options.excludeFromFastest || [];
+  var destDir  = options.destDir || path.resolve(process.cwd(), 'results');
+  var self = this;
 
   this.onRunComplete = function(browsers, resultInfo) {
+    var relativeResultSaver = new RelativeResultSaver(destDir);
     for (var browserName in resultSet) {
-      var groups = resultSet[browserName];
+      var suites = resultSet[browserName];
 
       this.write(browserName+'\n');
 
-      for (var groupName in groups) {
-        var results = groups[groupName]
-        if (results.length > 1) {
+      for (var suiteName in suites) {
+        var results = suites[suiteName];
+        var resultsCandidate = results.filter(function(result) {
+          return excludeFromFastest.indexOf(result.benchmark.name) === -1;
+        });
+        if (resultsCandidate.length > 1) {
           // Find the fastest among the groups
-          results.sort(function(a, b) {
+          resultsCandidate.sort(function(a, b) {
             return b.benchmark.hz - a.benchmark.hz;
           });
 
-          var fastest = results[0];
-          var secondFastest = results[1];
+          var fastest = resultsCandidate[0];
+          var secondFastest = resultsCandidate[1];
 
+          // classic result
           var timesFaster = (fastest.benchmark.hz/secondFastest.benchmark.hz).toFixed(2);
           this.write('  '+fastest.benchmark.suite+': '+fastest.benchmark.name+' at '+Math.floor(fastest.benchmark.hz)+' ops/sec ('+timesFaster+'x faster than '+secondFastest.benchmark.name+')\n');
         }
         else {
           this.write('  '+results[0].description+' had no peers for comparison at '+Math.floor(results[0].benchmark.hz)+' ops/sec\n')
         }
+
+        results
+          .map(function(result) {
+            return resultToRelativeResult(result, fastest);
+          })
+          .forEach(function(relativeResult) {
+            relativeResultSaver.save(suiteName, browserName, relativeResult);
+          });
       }
+
     }
+    // Save all results to disk
+    relativeResultSaver.flush(results)
+      .then(function() {
+        self.write(results.length + " files written to disk");
+      })
+      .catch(function(err) {
+        self.write('Writing files failed: ', err);
+      });
   };
 
   this.specSuccess = function(browser, result) {
-    var browser = browser.name;
-    var suite = result.benchmark.suite;
-    var name = result.benchmark.name;
+    var browserName = browser.name;
+    var suiteName = result.benchmark.suite;
+    var benchName = result.benchmark.name;
 
     // Get set and store results
-    var browserSet = resultSet[browser] = resultSet[browser] || {};
-    browserSet[suite] = browserSet[suite] || [];
-    browserSet[suite].push(result);
+    var browserSet = resultSet[browserName] = resultSet[browserName] || {};
+    browserSet[suiteName] = browserSet[suiteName] || [];
+    browserSet[suiteName].push(result);
 
-    this.write(browser+'  '+suite+': '+name+' at '+Math.floor(result.benchmark.hz)+' ops/sec\n');
+    this.write(browserName+'  '+suiteName+': '+benchName+' at '+Math.floor(result.benchmark.hz)+' ops/sec\n');
   };
 };
 
